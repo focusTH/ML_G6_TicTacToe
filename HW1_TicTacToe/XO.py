14efaddf01eaee7c6c80be6983622cd640a95876
Padonlabhat Sitthiparn <padonlabhatsit@gmail.com>
2022-03-09 14:34:36 +0700

    Update ansX

--- HW1_TicTacToe/XO.py
+++ HW1_TicTacToe/XO.py
@@ -5,5 +5,16 @@ print(ans[0],ans[1],ans[2]) #game
 print(ans[3],ans[4],ans[5]) #focus
 print(ans[6],ans[7],ans[8]) #focus
 
-value = input("Please enter a number:\n")
-print(f'You entered {value}')
+value = input("Please enter a number : ") #bun
+# print(f'You entered {value}')
+
+if value == "1":
+    ans[0] = "X" #focus
+elif value == "2":
+    ans[1] = "X" #focus
+
+
+
+print(ans[0],ans[1],ans[2]) #game
+print(ans[3],ans[4],ans[5]) #focus
+print(ans[6],ans[7],ans[8]) #focus
