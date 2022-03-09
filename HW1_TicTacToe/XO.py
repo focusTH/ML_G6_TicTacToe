f499ca2601e636d60078425446d5f8d1343f5f88
badstarss <boonsongprathip@gmail.com>
2022-03-09 14:21:52 +0700

    input

--- HW1_TicTacToe/XO.py
+++ HW1_TicTacToe/XO.py
@@ -4,3 +4,6 @@ ans=['1','2','3','4','5','6','7','8','9']
 print(ans[0],ans[1],ans[2]) #game
 print(ans[3],ans[4],ans[5]) #focus
 print(ans[6],ans[7],ans[8]) #focus
+
+value = input("Please enter a number:\n")
+print(f'You entered {value}')
