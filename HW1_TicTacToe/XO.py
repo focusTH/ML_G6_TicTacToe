bcfebf48950d5ac93f9db96e3d17a0d6f4d5fe9c
gamezzz121 <gamezzz121@gmail.com>
2022-03-11 11:33:50 +0700

    นับ A/B

--- HW1_TicTacToe/XO.py
+++ HW1_TicTacToe/XO.py
@@ -1,13 +1,177 @@
 import random as ran
-ans = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-
+ans = ['1', '2', '3', '4', 'O', '6', '7', '8', '9']
+A=0
+B=0
+Vs=0
+Vb=100
 print(ans[0], ans[1], ans[2])  # game
 print(ans[3], ans[4], ans[5])  # focus
 print(ans[6], ans[7], ans[8])  # focus
 
 
-def move(value,i):
+def move(value,i,A,B,Vs,Vb):
     if i == 1 or i == 3 or i == 5 or i == 7 or i == 9 :
+        # if ans[0]==1 :
+        #     if ans[1]==2 :
+        #         A=A+1
+        #     if ans[3]==4 :
+        #         A=A+1
+        #     if ans[1]!='X' :
+        #         B=B+1
+        #     if ans[3]!='X' :
+        #         B=B+1
+        #     print('A : ', A)
+        #     print('B : ', B)
+        #     Vb = A + B
+        #     print('Vb : ', Vb)
+        #
+        # if Vb < Vs :
+        #     Vs=Vb
+        #
+        # if ans[1] == 2:
+        #     if ans[0] == 1:
+        #         A = A + 1
+        #     if ans[4] == 5:
+        #         A = A + 1
+        #     if ans[2] == 3:
+        #         A = A + 1
+        #     if ans[0] != 'X':
+        #         B = B + 1
+        #     if ans[4] != 'X':
+        #         B = B + 1
+        #     if ans[2] != 'X':
+        #         B = B + 1
+        #     print('A : ', A)
+        #     print('B : ', B)
+        #     Vb = A + B
+        #     print('Vb : ', Vb)
+        #
+        # if Vb < Vs :
+        #     Vs=Vb
+        #
+        # if ans[2] == 3:
+        #     if ans[1] == 2:
+        #         A = A + 1
+        #     if ans[5] == 6:
+        #         A = A + 1
+        #
+        #     if ans[1] != 'X':
+        #         B = B + 1
+        #     if ans[5] != 'X':
+        #         B = B + 1
+        #
+        #     print('A : ', A)
+        #     print('B : ', B)
+        #     Vb = A + B
+        #     print('Vb : ', Vb)
+        #
+        # if Vb < Vs :
+        #     Vs=Vb
+        #
+        # if ans[3] == 4:
+        #     if ans[0] == 1:
+        #         A = A + 1
+        #     if ans[4] == 5:
+        #         A = A + 1
+        #     if ans[6] == 7:
+        #         A = A + 1
+        #
+        #     if ans[0] != 'X':
+        #         B = B + 1
+        #     if ans[4] != 'X':
+        #         B = B + 1
+        #     if ans[6] != 'X':
+        #         B = B + 1
+        #
+        #     print('A : ', A)
+        #     print('B : ', B)
+        #     Vb = A + B
+        #     print('Vb : ', Vb)
+        #
+        # if Vb < Vs :
+        #     Vs=Vb
+        #
+        # if ans[5] == 6:
+        #     if ans[2] == 3:
+        #         A = A + 1
+        #     if ans[4] == 5:
+        #         A = A + 1
+        #     if ans[8] == 9:
+        #         A = A + 1
+        #
+        #     if ans[2] != 'X':
+        #         B = B + 1
+        #     if ans[4] != 'X':
+        #         B = B + 1
+        #     if ans[8] != 'X':
+        #         B = B + 1
+        #
+        #     print('A : ', A)
+        #     print('B : ', B)
+        #     Vb = A + B
+        #     print('Vb : ', Vb)
+        #
+        # if Vb < Vs:
+        #     Vs = Vb
+        #
+        # if ans[6] == 7:
+        #     if ans[3] == 4:
+        #         A = A + 1
+        #     if ans[7] == 8:
+        #         A = A + 1
+        #
+        #
+        #     if ans[3] != 'X':
+        #         B = B + 1
+        #     if ans[7] != 'X':
+        #         B = B + 1
+        #
+        #     print('A : ', A)
+        #     print('B : ', B)
+        #     Vb = A + B
+        #     print('Vb : ', Vb)
+        #
+        # if Vb < Vs:
+        #     Vs = Vb
+        #
+        # if ans[7] == 8:
+        #     if ans[4] == 5:
+        #         A = A + 1
+        #     if ans[6] == 7:
+        #         A = A + 1
+        #     if ans[8] == 9:
+        #         A = A + 1
+        #
+        #     if ans[4] != 'X':
+        #         B = B + 1
+        #     if ans[6] != 'X':
+        #         B = B + 1
+        #     if ans[8] != 'X':
+        #         B = B + 1
+        #
+        #     print('A : ', A)
+        #     print('B : ', B)
+        #     Vb = A + B
+        #     print('Vb : ', Vb)
+        #
+        # if Vb < Vs:
+        #     Vs = Vb
+        #
+        # if ans[8] == 9:
+        #     if ans[5] == 5:
+        #         A = A + 1
+        #     if ans[7] == 7:
+        #         A = A + 1
+        #
+        #     if ans[5] != 'X':
+        #         B = B + 1
+        #     if ans[7] != 'X':
+        #         B = B + 1
+        #
+        #     print('A : ', A)
+        #     print('B : ', B)
+        #     Vb = A + B
+        #     print('Vb : ', Vb)
         if value == "1":
             ans[0] = "X"  # focus
         elif value == "2":
@@ -26,27 +190,25 @@ def move(value,i):
             ans[7] = "X"  # focus
         elif value == "9":
             ans[8] = "X"  # focus
-    else : #bot Game
-
-        print('')
-        # if value == "1":
-        #     ans[0] = "O"
-        # elif value == "2":
-        #     ans[1] = "O"
-        # elif value == "3":
-        #     ans[2] = "O"
-        # elif value == "4":
-        #     ans[3] = "O"
-        # elif value == "5":
-        #     ans[4] = "O"
-        # elif value == "6":
-        #     ans[5] = "O"
-        # elif value == "7":
-        #     ans[6] = "O"
-        # elif value == "8":
-        #     ans[7] = "O"
-        # elif value == "9":
-        #     ans[8] = "O"
+    else :
+        if value == "1":
+            ans[0] = "X"  # focus
+        elif value == "2":
+            ans[1] = "X"  # focus
+        elif value == "3":
+            ans[2] = "X"  # focus
+        elif value == "4":
+            ans[3] = "X"  # focus
+        elif value == "5":
+            ans[4] = "X"  # focus
+        elif value == "6":
+            ans[5] = "X"  # focus
+        elif value == "7":
+            ans[6] = "X"  # focus
+        elif value == "8":
+            ans[7] = "X"  # focus
+        elif value == "9":
+            ans[8] = "X"  # focus
 
 
     print(ans[0], ans[1], ans[2])  # game
@@ -96,7 +258,7 @@ while space_exist():
 
     print('round ',i)
     value = input("Please enter a number : ")  # bun
-    value = move(value,i)  # O
+    value = move(value,i,A,B,Vs,Vb)  # O
     i=i+1
     won = win()  # O
 
